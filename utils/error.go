109c5b2eeda68acc5a4148e61cc4651d532b34bf
jgoudiab <jeangabrielgoudiaby@gmail.com>
2024-09-18 15:29:18 +0000

    ajout de test fail pour le download

--- utils/error.go
+++ utils/error.go
@@ -16,3 +16,29 @@ func PrintUsageAndExit() {
 	fmt.Println("Usage: go run main.go [-P destination_dir] [-i input_file] [--rate-limit rate] <URL>")
 	os.Exit(1)
 }
+
+/* 
+package download
+
+import "fmt"
+
+type DownloadError struct {
+	Message string
+	Cause   error
+}
+
+func (e *DownloadError) Error() string {
+	if e.Cause != nil {
+		return fmt.Sprintf("%s: %v", e.Message, e.Cause)
+	}
+	return e.Message
+}
+
+func NewDownloadError(message string, cause error) *DownloadError {
+	return &DownloadError{
+		Message: message,
+		Cause:   cause,
+	}
+}
+
+*/
